7aa51ebd3a655d447b23df2a8483ebfa27c44aec
abirc8010 <abirc8010@gmail.com>
2024-04-29 14:03:54 +0530

    Improved home page features

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -80,89 +80,37 @@ export default function InputWithIcon() {
         <div className='heading'>WebChat</div>
         <Box className="form-box">
           <Box sx={{ borderBottom: 1, borderColor: 'divider', width: '100%' }}>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              aria-label="basic tabs example"
-            >
-              <Tab
-                label="Sign Up"
-                {...a11yProps(0)}
-                className='tab'
-                sx={{ color: 'rgb(255, 255, 255)' }}
-              />
-              <Tab
-                label="Login"
-                {...a11yProps(1)}
-                className='tab'
-                sx={{ color: 'rgb(255, 255, 255)' }}
-              />
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+              <Tab label="Sign Up" {...a11yProps(0)} className='tab' sx={{ color: 'rgb(255, 255, 255)' }} />
+              <Tab label="Login" {...a11yProps(1)} className='tab' sx={{ color: 'rgb(255, 255, 255)' }} />
             </Tabs>
           </Box>
-
           <CustomTabPanel value={value} index={0} className='tab-panel'>
             <form onSubmit={handleSubmit} >
               <Notification openDialog={openDialog} handleCloseDialog={handleCloseDialog} />
               <TextField label="Username" sx={{ mb: 2 }} className="text-field" onChange={(e) => setUsername(e.target.value)} fullWidth required />
               <TextField label="Email" sx={{ mb: 2 }} className="text-field" onChange={(e) => setEmail(e.target.value)} type="email" fullWidth />
-              <TextField
-                label="Password"
-                sx={{ mb: 2, width: '100%' }}
-                className="text-field"
-                onChange={(e) => setPassword(e.target.value)}
-                type={showPassword ? 'text' : 'password'}
-                fullWidth
-                InputProps={{
-                  endAdornment: (
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={() => setShowPassword((prev) => !prev)}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                    </IconButton>
-                  ),
-                }}
-              />
-
+              <TextField label="Password" sx={{ mb: 2, width: '100%' }} className="text-field" onChange={(e) => setPassword(e.target.value)} type={showPassword ? 'text' : 'password'} fullWidth InputProps={{ endAdornment: (<IconButton aria-label="toggle password visibility" onClick={() => setShowPassword((prev) => !prev)} edge="end">{showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}</IconButton>) }} />
               <div className='btn'><Button type="submit" sx={{ width: '60%', mb: 2 }} variant="contained" color="primary" fullWidth>SignUp</Button></div>
             </form>
+            <Typography variant="h6" sx={{ mt: -1, mb: 1, color: "white" }}>-------- OR --------</Typography>
             <button className="login-with-google-btn">Sign In with google</button>
+            <div style={{ color: "white",width:"200px",marginTop:"10px"}}>Already have an account? <div style={{cursor:"pointer", textDecoration:"underline"}}onClick={()=>{setValue(1)}}>Login</div></div>
           </CustomTabPanel>
           <CustomTabPanel value={value} index={1} className="tab-panel">
             <form onSubmit={handleSubmit}>
               <div className="form-fields">
                 <TextField label="Email" sx={{ mb: 2, width: '100%' }} className="text-field" onChange={(e) => setEmail(e.target.value)} type="email" fullWidth />
-                <TextField
-                  label="Password"
-                  sx={{ mb: 2, width: '100%' }}
-                  className="text-field"
-                  onChange={(e) => setPassword(e.target.value)}
-                  type={showPassword ? 'text' : 'password'}
-                  fullWidth
-                  InputProps={{
-                    endAdornment: (
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={() => setShowPassword((prev) => !prev)}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                      </IconButton>
-                    ),
-                  }}
-                />
-
+                <TextField label="Password" sx={{ mb: 2, width: '100%' }} className="text-field" onChange={(e) => setPassword(e.target.value)} type={showPassword ? 'text' : 'password'} fullWidth InputProps={{ endAdornment: (<IconButton aria-label="toggle password visibility" onClick={() => setShowPassword((prev) => !prev)} edge="end">{showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}</IconButton>) }} />
                 <Button type="submit" sx={{ width: '60%', mb: 2 }} variant="contained" color="primary" fullWidth>Login</Button>
               </div>
             </form>
+            <Typography variant="h6" sx={{ mt: -1, mb: 1, color: "white" }}>-------- OR --------</Typography>
             <button className="login-with-google-btn">Sign In with google</button>
+            <div style={{ color: "white",width:"200px",marginTop:"10px"}}>Don't have an account? <div style={{cursor:"pointer", textDecoration:"underline"}}onClick={()=>{setValue(0)}}>SignUp</div></div>
           </CustomTabPanel>
-
         </Box>
-      </div >
-
-
+      </div>
     </>
   );
 }
